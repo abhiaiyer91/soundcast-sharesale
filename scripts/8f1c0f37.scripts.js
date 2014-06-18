fdc6cf49b5b86495e1709ce6ea1a25da3f2dbb20
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-06-18 12:03:46 -0700

    gh-pages

---
+++ scripts/8f1c0f37.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("sharesaleApp",["ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("sharesaleApp").controller("MainCtrl",["$scope",function(a){a.title="share sale soundcast test"}]);
